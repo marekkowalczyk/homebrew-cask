a8c33751015a2528b94e51b6dd4622120de98e22
Marek Kowalczyk <marek.kowalczyk@mandarine.pl>
2021-03-21 21:34:27 +0100

    Update phoenix-slides.rb
    Delete empty lines

--- Casks/phoenix-slides.rb
+++ Casks/phoenix-slides.rb
@@ -1,12 +1,10 @@
 cask "phoenix-slides" do
   version "1.4.5"
   sha256 "78c491f543abaef0da62fc892433d9698f6f5badd98210d0ac4c4865d289cdd6"
-
   url "https://github.com/gobbledegook/creevey/releases/download/v#{version}/phoenix-slides-#{version.no_dots}.dmg",
       verified: "github.com/gobbledegook/creevey/"
   name "Phoenix Slides"
   homepage "https://blyt.net/phxslides/"
-  desc "Full-screen slideshow program/image browser"
-  
+  desc "Full-screen slideshow program/image browser"  
   app "Phoenix Slides.app"
 end
